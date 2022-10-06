fc2e444b3297a520624d8c164181bc85390aa0b2
mhhasanmehedi <mmmehedi53@gmail.com>
2022-10-06 15:44:52 +0600

    first commit

---
+++ src/components/ClickCounter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ClickCounter = ({ count, incrementCount }) => {
+  return (
+    <div>
+      <button onClick={incrementCount}>Clicked {count} times</button>
+    </div>
+  );
+};
+
+export default ClickCounter;
